a6ed5feeb9041fe68b282e8b3c896816611365b3
Vita Levon <w.iv.von@gmail.com>
2026-06-20 19:35:00 +0300

    add git commit

---
+++ js/home-work-js/home-work-js-01/task-05/file1.js
@@ -0,0 +1,7 @@
+ let value1 = "hello";
+ let value2 = 'Vita';
+
+ let value3 = value1 + " " + value2; // конкатенація – зчеплення рядків. В результаті конкатенації у змінній
+        // value3 запишеться значення "hello world";
+        console.log(value3);
+
