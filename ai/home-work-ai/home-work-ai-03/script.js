5a7a42283071bca3d8c305479acfb548b99b24b8
Vita Levon <w.iv.von@gmail.com>
2026-08-25 11:30:33 +0300

    add

--- ai/home-work-ai/home-work-ai-03/script.js
+++ ai/home-work-ai/home-work-ai-03/script.js
@@ -6,6 +6,9 @@ const result = document.getElementById('result');
 
 const apiKey = "AQ.Ab8RN6JkcghFfmv_IDfpIeaMFJcnntMhjzw7mEHh5iSIwbQbpg";
 
+const API_URL =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     const promptValue = prompt.value;
