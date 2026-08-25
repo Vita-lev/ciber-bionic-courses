387ab41159b45929315992d878ae9eb942fc1cd3
Vita Levon <w.iv.von@gmail.com>
2026-08-25 11:28:47 +0300

    add

--- ai/home-work-ai/home-work-ai-03/script.js
+++ ai/home-work-ai/home-work-ai-03/script.js
@@ -2,6 +2,10 @@ const form = document.getElementById('form');
 const prompt = document.getElementById('prompt');
 const result = document.getElementById('result');
 
+
+
+const apiKey = "AQ.Ab8RN6JkcghFfmv_IDfpIeaMFJcnntMhjzw7mEHh5iSIwbQbpg";
+
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     const promptValue = prompt.value;
